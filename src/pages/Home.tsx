b326bad27024791a660d2bfbfaa2045abc7dca4a
ahmedalinahid243-beep <ahmedalinahid243@gmail.com>
2026-09-02 21:42:16 +0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,23 +11,24 @@ import EventsPreview from "../components/home/EventsPreview";
 import FaqSection from "../components/home/FaqSection";
 import DisclaimerPreview from "../components/home/DisclaimerPreview";
 import FinalCTA from "../components/home/FinalCTA";
+import ScrollReveal from "../components/ScrollReveal";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
-      <TraderProfile />
-      <HowItWorks />
-      <Features />
-      <Results />
-      <TelegramCTA />
-      <QuotexSection />
-      <TutorialVideo />
-      <EventsPreview />
-      <FaqSection />
-      <DisclaimerPreview />
-      <FinalCTA />
+      <ScrollReveal><Stats /></ScrollReveal>
+      <ScrollReveal><TraderProfile /></ScrollReveal>
+      <ScrollReveal><HowItWorks /></ScrollReveal>
+      <ScrollReveal><Features /></ScrollReveal>
+      <ScrollReveal><Results /></ScrollReveal>
+      <ScrollReveal><TelegramCTA /></ScrollReveal>
+      <ScrollReveal><QuotexSection /></ScrollReveal>
+      <ScrollReveal><TutorialVideo /></ScrollReveal>
+      <ScrollReveal><EventsPreview /></ScrollReveal>
+      <ScrollReveal><FaqSection /></ScrollReveal>
+      <ScrollReveal><DisclaimerPreview /></ScrollReveal>
+      <ScrollReveal><FinalCTA /></ScrollReveal>
     </>
   );
 }
