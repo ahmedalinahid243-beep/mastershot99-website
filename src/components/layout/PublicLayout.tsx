8f191cc6e3ce1a52b8b51efabc3637c6a9327d15
ahmedalinahid243-beep <ahmedalinahid243@gmail.com>
2026-09-04 06:58:50 +0600

    Update PublicLayout.tsx

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -2,10 +2,12 @@ import type { ReactNode } from "react";
 import AnnouncementTicker from "./AnnouncementTicker";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SplashScreen from "./SplashScreen";
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <SplashScreen />
       <AnnouncementTicker />
       <Navbar />
       <main className="flex-1">{children}</main>
